3928df984480c3988b5081f89cee4695b3b7863a
Sargun Vohra <sargun.vohra@gmail.com>
2018-09-15 03:52:18 -0700

    Fix sed

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ set -o pipefail
 set -x
 
 yarn run netlify-lambda build functions
-sed -i "s/\$BASE_URL/$BASE_URL/" _functions/*.js
+sed -i "s,\$BASE_URL,$BASE_URL," _functions/*.js
 ditto transform --base-url="$BASE_URL" --dest-dir='_dist'
