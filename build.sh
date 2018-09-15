2388dfeeefc6a6945149290ac424ba0738d0e8fb
Sargun Vohra <sargun.vohra@gmail.com>
2018-09-15 00:57:01 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -4,8 +4,5 @@ set -e
 set -o pipefail
 set -x
 
-yarn install
 yarn run netlify-lambda build functions
-
-poetry install --no-dev
-poetry run ditto transform --base-url='https://pokeapi-prod.netlify.com/' --dest-dir='_dist'
+ditto transform --base-url='https://pokeapi-prod.netlify.com/' --dest-dir='_dist'
