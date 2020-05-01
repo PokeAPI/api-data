504c0e2704d2c3cb63c15a52319597bd0d7ba5ce
Alessandro Pezzè <igougi.ui@gmail.com>
2020-05-01 22:58:22 +0200

    play: staging deployment

---
+++ scripts/transform.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Runs in CircleCI
+# Replaces all the http://localhost links of api-data to match the corresponding deployment location.
+
+if [ "${CIRCLE_BRANCH}" = 'master' ]; then # https://stackoverflow.com/a/2013589/3482533
+    ~/.local/bin/ditto transform --base-url='https://pokeapi.co' --src-dir=data --dest-dir=_gen
+elif [ "${CIRCLE_BRANCH}" = 'test-staging' ]; then # Change to `staging`
+    ~/.local/bin/ditto transform --base-url='https://pokeapi-test-b6137.firebaseapp.com' --src-dir=data --dest-dir=_gen
+fi
