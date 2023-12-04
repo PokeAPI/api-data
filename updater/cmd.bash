5b38016f2be4193939e6719146b581bf759e6d8d
Naramsim <igougi.ui@gmail.com>
2023-12-04 10:28:55 +0100

    feat: use compose v2/bump deps/commitandpush conditionally/add cicd

--- updater/cmd.bash
+++ updater/cmd.bash
@@ -7,12 +7,12 @@
 [ -z "${REPO_DATA}" ] && { echo "Need to set REPO_DATA"; exit 1; }
 [ -z "${BRANCH_NAME}" ] && { echo "Need to set BRANCH_NAME"; exit 1; }
 [ -z "${REPO_POKEAPI_CHECKOUT_OBJECT:=master}" ] && { echo "REPO_POKEAPI_CHECKOUT_OBJECT not set, defaulting to \`master\`"; }
+[ -z "${TEST:=true}" ]
+declare -r COMMIT_AND_PUSH="${COMMIT_AND_PUSH:-false}"
 
 set -e
 set -o pipefail
 set -x
-
-export COMPOSE_INTERACTIVE_NO_CLI=1
 
 dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &> /dev/null &
 
@@ -25,12 +25,10 @@ git checkout "$REPO_POKEAPI_CHECKOUT_OBJECT"
 git submodule init
 git submodule update --remote
 
-docker volume create --name=redis_data
-docker volume create --name=pg_data
-docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d
+docker compose -f docker-compose.yml -f docker-compose-dev.yml up -d
 
-docker-compose exec -T app python manage.py migrate --settings=config.docker-compose
-docker-compose exec -T app sh -c 'echo "from data.v2.build import build_all; build_all()" | python manage.py shell --settings=config.docker-compose'
+docker compose exec -T app python manage.py migrate --settings=config.docker-compose
+docker compose exec -T app sh -c 'echo "from data.v2.build import build_all; build_all()" | python manage.py shell --settings=config.docker-compose'
 
 # set up the data side
 cd ../api-data
@@ -46,11 +44,13 @@ ditto clone --src-url http://localhost/ --dest-dir ./data --select pokemon/129
 ditto analyze --data-dir ./data
 
 # commit and push
-git add data
-git config user.name "$COMMIT_NAME"
-git config user.email "$COMMIT_EMAIL"
-if ! git commit -m "$COMMIT_MESSAGE"; then
-    echo "The generated data doesn't bring any updates"
-    exit 2
+if [ "$COMMIT_AND_PUSH" = 'true' ]; then
+    git add data
+    git config user.name "$COMMIT_NAME"
+    git config user.email "$COMMIT_EMAIL"
+    if ! git commit -m "$COMMIT_MESSAGE"; then
+        echo "The generated data doesn't bring any updates"
+        exit 2
+    fi
+    git push -fu origin "$BRANCH_NAME"
 fi
-git push -fu origin "$BRANCH_NAME"
