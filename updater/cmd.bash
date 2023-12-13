9042bd568cca7fd71964b24a18f1e29ab6b81c27
Alessandro Pezzè <igougi.ui@gmail.com>
2023-12-13 16:24:16 +0900

    #201 continuation (#202)
    Co-authored-by: pokeapi-machine-user <pokeapi.co@gmail.com>

--- updater/cmd.bash
+++ updater/cmd.bash
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-[ -z "${COMMIT_NAME}" ] && { echo "Need to set COMMIT_NAME"; exit 1; }
-[ -z "${COMMIT_EMAIL}" ] && { echo "Need to set COMMIT_EMAIL"; exit 1; }
-[ -z "${COMMIT_MESSAGE}" ] && { echo "Need to set COMMIT_MESSAGE"; exit 1; }
 [ -z "${REPO_POKEAPI}" ] && { echo "Need to set REPO_POKEAPI"; exit 1; }
 [ -z "${REPO_DATA}" ] && { echo "Need to set REPO_DATA"; exit 1; }
 [ -z "${BRANCH_NAME}" ] && { echo "Need to set BRANCH_NAME"; exit 1; }
 [ -z "${REPO_POKEAPI_CHECKOUT_OBJECT:=master}" ] && { echo "REPO_POKEAPI_CHECKOUT_OBJECT not set, defaulting to \`master\`"; }
+[ -z "${REPO_APIDATA_CHECKOUT_OBJECT:=master}" ] && { echo "REPO_APIDATA_CHECKOUT_OBJECT not set, defaulting to \`master\`"; }
+[ -z "${TEST:=true}" ]
+declare -r COMMIT_AND_PUSH="${COMMIT_AND_PUSH:-false}"
 
 set -e
 set -o pipefail
 set -x
 
-export COMPOSE_INTERACTIVE_NO_CLI=1
-
 dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &> /dev/null &
 
 git clone "$REPO_POKEAPI" pokeapi
-git clone --depth=1 "$REPO_DATA" api-data
+git clone "$REPO_DATA" api-data
 
 # set up the pokeapi side
 cd pokeapi
@@ -25,15 +23,13 @@ git checkout "$REPO_POKEAPI_CHECKOUT_OBJECT"
 git submodule init
 git submodule update --remote
 
-docker volume create --name=redis_data
-docker volume create --name=pg_data
-docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d
-
-docker-compose exec -T app python manage.py migrate --settings=config.docker-compose
-docker-compose exec -T app sh -c 'echo "from data.v2.build import build_all; build_all()" | python manage.py shell --settings=config.docker-compose'
+docker compose -f docker-compose.yml -f docker-compose-dev.yml up -d
+docker compose exec -T app python manage.py migrate --settings=config.docker-compose
+docker compose exec -T app sh -c 'echo "from data.v2.build import build_all; build_all()" | python manage.py shell --settings=config.docker-compose'
 
 # set up the data side
 cd ../api-data
+git checkout "$REPO_APIDATA_CHECKOUT_OBJECT"
 git branch -D "$BRANCH_NAME" || true
 git branch "$BRANCH_NAME"
 git checkout "$BRANCH_NAME"
@@ -46,11 +42,16 @@ ditto clone --src-url http://localhost/ --dest-dir ./data --select pokemon/129
 ditto analyze --data-dir ./data
 
 # commit and push
-git add data
-git config user.name "$COMMIT_NAME"
-git config user.email "$COMMIT_EMAIL"
-if ! git commit -m "$COMMIT_MESSAGE"; then
-    echo "The generated data doesn't bring any updates"
-    exit 2
+if [ "$COMMIT_AND_PUSH" = 'true' ]; then
+    [ -z "${COMMIT_NAME}" ] && { echo "Need to set COMMIT_NAME"; exit 1; }
+    [ -z "${COMMIT_EMAIL}" ] && { echo "Need to set COMMIT_EMAIL"; exit 1; }
+    [ -z "${COMMIT_MESSAGE}" ] && { echo "Need to set COMMIT_MESSAGE"; exit 1; }
+    git add data
+    git config user.name "$COMMIT_NAME"
+    git config user.email "$COMMIT_EMAIL"
+    if ! git commit -m "$COMMIT_MESSAGE"; then
+        echo "The generated data doesn't bring any updates"
+        exit 2
+    fi
+    git push -fu origin "$BRANCH_NAME"
 fi
-git push -fu origin "$BRANCH_NAME"
